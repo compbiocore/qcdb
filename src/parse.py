efab46b084e3c2cb710d9ef32a2770fd2adc158e
Joselynn Wallace <joselynnw@gmail.com>
2019-03-05 10:49:51 -0500

    refactor(src/Parse.Py): parse.py requires an argument (-d) indicating file paths to inputs, also parses sample_id based on file name, not file path
    changed the parsing behavior as per Ashok's request that qckitfastq output filenames indicate the fastq files that were used as the input

--- src/parse.py
+++ src/parse.py
@@ -1,85 +1,184 @@
 import pandas as pd
+import glob2
+import argparse
+import os
 
-
-#The outputs from bioflows sra-tools will prepend the .fastq filenames with information about SRA experiments, etc.
-#This information will also be parsed by qckitfastq (needs to be updated) so that it outputs files containing SRA experiments, etc. in the filenames.
-#Once this is confirmed (bioflows) and implemented (qckitfastq), the parsing should be changed so that the filenames are parsed (not the directories). 
-
-fastqckit_dir = ('../../SRX3544048/SAMN08331802/SRR6453154') #assign fastqckit_dir to the folder where the adapter content files are saved
-fastqckit_dir.split('/') #split the directory string, dir names are informative -- they correspond to experiment/biosample/run
-experiment=fastqckit_dir.split('/')[-3]
-biosample=fastqckit_dir.split('/')[-2]
-run=fastqckit_dir.split('/')[-1]
+parser=argparse.ArgumentParser()
+parser.add_argument('-d', nargs=1, required=True, dest='directory', help='path to qckitfastq output directory')
+args=parser.parse_args()
 
 #adapters
-adapter_content_file = ('/adapter_content.csv') #specify which is the adapter content file
-adapter_content_file_path = fastqckit_dir+adapter_content_file #join the 2 strings to specify the full path name
-adapter_content = pd.read_csv(adapter_content_file_path) #read in the adapter content as a df
-adapter_content = adapter_content[['adapter','count']] #clean up superfluous column
-adapter_content['sample_id']=experiment+'_'+biosample+'_'+run
+adapter_content_files = glob2.glob(args.directory[0]+'*_adapter_content.csv') 
+adapter_content_table=pd.DataFrame()
+for adapter_file in adapter_content_files:
+    base_file=os.path.basename(adapter_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    adapter_content=pd.read_table(adapter_file, sep=',')
+    adapter_content['sample_id']=sample+'_'+experiment   
+    if library_read_type == '1':
+        adapter_content['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        adapter_content['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        adapter_content['library_read_type'] = 'se'
+    adapter_content_table=adapter_content_table.append(adapter_content, ignore_index=True)
+    
+
+#GC content
+gc_content_files = glob2.glob(args.directory[0]+'*_gc_content.csv') 
+gc_content_table=pd.DataFrame()
+for gc_file in gc_content_files:
+    base_file=os.path.basename(gc_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    gc_content=pd.read_table(gc_file, sep=',')
+    gc_content = gc_content[['read','mean_GC']]
+    gc_content['sample_id']=sample+'_'+experiment 
+    if library_read_type == '1':
+        gc_content['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        gc_content['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        gc_content['library_read_type'] = 'se'
+    gc_content_table=gc_content_table.append(gc_content, ignore_index=True)
 
 
-#gc content
-gc_content_file = ('/gc_content.csv') #specify which is the gc content file
-gc_content_file_path = fastqckit_dir+gc_content_file #join the 2 strings to specify the full path name
-gc_content = pd.read_csv(gc_content_file_path) #read in the gc content as a df
-gc_content = gc_content[['read','mean_GC']] #clean up superfluous column
-gc_content['sample_id']=experiment+'_'+biosample+'_'+run
+#overrep kmer
+overrep_kmer_files = glob2.glob(args.directory[0]+'*_overrep_kmer.csv') 
+overrep_kmer_table=pd.DataFrame()
+for overrep_kmer_file in overrep_kmer_files:
+    base_file=os.path.basename(overrep_kmer_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    overrep_kmer=pd.read_table(overrep_kmer_file, sep=',')
+    overrep_kmer = overrep_kmer[['row','position','obsexp_ratio','kmer']]
+    overrep_kmer['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        overrep_kmer['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        overrep_kmer['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        overrep_kmer['library_read_type'] = 'se'         
+    overrep_kmer_table=overrep_kmer_table.append(overrep_kmer, ignore_index=True)
 
+#kmer counts 
+kmer_counts_files = glob2.glob(args.directory[0]+'*kmer_count.csv') 
+kmer_counts_table=pd.DataFrame()
+for kmer_counts_file in kmer_counts_files:
+    base_file=os.path.basename(kmer_counts_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    kmer_counts=pd.read_table(kmer_counts_file, sep=',')
+    kmer_counts.rename(columns={kmer_counts.columns[0]:'kmer'}, inplace=True) #name the first 0-indexed column 'kmer'
+    kmer_counts = pd.melt(kmer_counts, id_vars=['kmer']) #melt the dataframe but specify that the id_vars is the 'kmer' column
+    kmer_counts['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        kmer_counts['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        kmer_counts['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        kmer_counts['library_read_type'] = 'se'         
+    kmer_counts_table=kmer_counts_table.append(kmer_counts, ignore_index=True)    
+    
 
-#kmer counts
-kmer_content_file = ('/kmer_count.csv') #specify which is the kmer content file
-kmer_content_file_path = fastqckit_dir+kmer_content_file #join the 2 strings to specify the full path name
-kmer_content = pd.read_csv(kmer_content_file_path) #read in the kmer content as a df
-kmer_content.rename(columns={kmer_content.columns[0]:'kmer'}, inplace=True) #name the first 0-indexed column 'kmer'
-kmer_content = pd.melt(kmer_content, id_vars=['kmer']) #melt the dataframe but specify that the id_vars is the 'kmer' column
-kmer_content.columns=['kmer','position','count'] #give the columns the right names
-kmer_content['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
-
-#overrepresented kmers
-overrep_kmer_file = ('/overrep_kmer.csv') #specify which is the overrepresented kmer file
-overrep_kmer_file_path = fastqckit_dir+overrep_kmer_file #join the 2 strings to specify the full path name
-overrep_kmer_content = pd.read_csv(overrep_kmer_file_path) #read in the overrepresented kmer file as a df
-overrep_kmer_content = overrep_kmer_content[['row','position','obsexp_ratio','kmer']] #clean up superfluous column
-overrep_kmer_content['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
-
-#overrepresented reads
-overrep_reads_file = ('/overrep_reads.csv') #specify which is the overrepresented reads file
-overrep_reads_file_path = fastqckit_dir+overrep_reads_file #join the 2 strings to specify the full path name
-overrep_reads = pd.read_csv(overrep_reads_file_path) #read in the overrepresented reads file as a df
-overrep_reads = overrep_reads[['read_sequence','count']] #clean up superfluous column
-overrep_reads['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
-
+##overrepresented reads
+overrep_reads_files = glob2.glob(args.directory[0]+'*overrep_reads.csv') 
+overrep_reads_table=pd.DataFrame()
+for overrep_reads_file in overrep_reads_files:
+    base_file=os.path.basename(overrep_reads_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    overrep_reads=pd.read_table(overrep_reads_file, sep=',')
+    overrep_reads['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        overrep_reads['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        overrep_reads['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        overrep_reads['library_read_type'] = 'se'         
+    overrep_reads_table=overrep_reads_table.append(overrep_reads, ignore_index=True)  
+    
+    
 #per base quality
-per_base_qual_file = ('/per_base_quality.csv') #specify which is the overrepresented reads file
-per_base_qual_file_path = fastqckit_dir+per_base_qual_file #join the 2 strings to specify the full path name
-per_base_qual = pd.read_csv(per_base_qual_file_path) #read in the overrepresented reads file as a df
-per_base_qual=per_base_qual[['position', 'q10','q25','median','q75','q90']]
-per_base_qual['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
+per_base_quality_files = glob2.glob(args.directory[0]+'*per_base_quality.csv') 
+per_base_quality_table=pd.DataFrame()
+for per_base_quality_file in per_base_quality_files:
+    base_file=os.path.basename(per_base_quality_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    per_base_quality=pd.read_table(per_base_quality_file, sep=',')
+    per_base_quality=per_base_quality[['position', 'q10','q25','median','q75','q90']]
+    per_base_quality['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        per_base_quality['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        per_base_quality['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        per_base_quality['library_read_type'] = 'se'         
+    per_base_quality_table=per_base_quality_table.append(per_base_quality, ignore_index=True)      
+    
 
 #per read quality
-per_read_qual_file = ('/per_read_quality.csv') #specify which is the overrepresented reads file
-per_read_qual_file_path = fastqckit_dir+per_read_qual_file #join the 2 strings to specify the full path name
-per_read_qual = pd.read_csv(per_read_qual_file_path) #read in the overrepresented reads file as a df
-per_read_qual=per_read_qual[['read','sequence_mean']]
-per_read_qual['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
-
+per_read_quality_files = glob2.glob(args.directory[0]+'*per_read_quality.csv') 
+per_read_quality_table=pd.DataFrame()
+for per_read_quality_file in per_read_quality_files:
+    base_file=os.path.basename(per_read_quality_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    per_read_quality=pd.read_table(per_read_quality_file, sep=',')
+    per_read_quality=per_read_quality[['read','sequence_mean']]
+    per_read_quality['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        per_read_quality['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        per_read_quality['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        per_read_quality['library_read_type'] = 'se'         
+    per_read_quality_table=per_read_quality_table.append(per_read_quality, ignore_index=True)  
+    
 #read content
-read_content_file = ('/read_content.csv') #specify which is the overrepresented reads file
-read_content_file_path = fastqckit_dir+read_content_file #join the 2 strings to specify the full path name
-read_content = pd.read_csv(read_content_file_path) #read in the overrepresented reads file as a df
-read_content=read_content[['position','a','c','t','g','n']]
-read_content['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
-
+read_content_files = glob2.glob(args.directory[0]+'*read_content.csv') 
+read_content_table=pd.DataFrame()
+for read_content_file in read_content_files:
+    base_file=os.path.basename(read_content_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    read_content=pd.read_table(read_content_file, sep=',')
+    read_content=read_content[['position','a','c','t','g','n']]
+    read_content['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        read_content['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        read_content['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        read_content['library_read_type'] = 'se'         
+    read_content_table=read_content_table.append(read_content, ignore_index=True)  
 
 #read length
-read_length_file = ('/read_length.csv') #specify which is the overrepresented reads file
-read_length_file_path = fastqckit_dir+read_length_file #join the 2 strings to specify the full path name
-read_length = pd.read_csv(read_length_file_path) #read in the overrepresented reads file as a df
-read_length=read_length[['read_length', 'num_reads']]
-read_length['sample_id']=experiment+'_'+biosample+'_'+run #add the sample_id column
+read_length_files = glob2.glob(args.directory[0]+'*read_length.csv') 
+read_length_table=pd.DataFrame()
+for read_length_file in read_length_files:
+    base_file=os.path.basename(read_length_file)
+    sample=base_file.split('_')[0]
+    experiment=base_file.split('_')[1]
+    library_read_type=base_file.split('_')[2]
+    read_length=pd.read_table(read_length_file, sep=',')
+    read_length=read_length[['read_length', 'num_reads']]
+    read_length['sample_id']=sample+'_'+experiment
+    if library_read_type == '1':
+        read_length['library_read_type'] = 'pe_1'
+    if library_read_type == '2':
+        read_length['library_read_type'] = 'pe_2'
+    if library_read_type.isalpha():
+        read_length['library_read_type'] = 'se'         
+    read_length_table=read_length_table.append(read_length, ignore_index=True)  
+   
