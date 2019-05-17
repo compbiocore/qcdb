7cba611e175b6f7bd98322af1b57e840d335d538
Joselynn Wallace <joselynnw@gmail.com>
2019-05-17 09:19:10 -0400

    refactor(In-reference-to-issue-#8): picard outputs are parsed as json files, also uses baseparser from parse.py

--- qcdb/parsers/picardtools_parse.py
+++ qcdb/parsers/picardtools_parse.py
@@ -18,52 +18,35 @@ class picardtoolsParser(BaseParser):
     def __init__(self, file_handle):
         log.info("Initializing picardtoolsParser...")
         BaseParser.__init__(self,file_handle)
-        self.alignmentmetrics_parse(file_handle)
+        self.parse(file_handle)
 
-    def alignmentmetrics_parse(self, file):
+#file=('/Users/jwalla12/Dropbox/SRS777777_SRX777777_alignment_summary_metrics.txt')
+
+    def parse(self, file):
         #dictionary_list = []         
         f = open(file, 'r', encoding = "ISO-8859-1")           
         contents = f.readlines()    
         for i, content in enumerate(contents):
-            if 'PAIR' in content:
-                row = content.strip("\n").split("\t")
-            elif 'UNPAIRED' in content:
-                row = content.strip("\n").split("\t")   
             if 'METRICS CLASS' in content:
                 qc_program = content.strip("\n").split(".")[-3].split("\t")[-1] 
                 qc_metric = content.strip("\n").split(".")[-1]    
                 header = (contents[i+1]).strip("\n").split("\t")
+                
+                if qc_metric == 'AlignmentSummaryMetrics':
+                    if 'PAIR' in content:
+                        row = content.strip("\n").split("\t")
+                    elif 'UNPAIRED' in content:
+                        row = content.strip("\n").split("\t")   
+                        
+                elif (qc_metric == 'InsertSizeMetrics') or (qc_metric == 'GcBiasSummaryMetrics'):
+                    row = (contents[i+2]).strip("\n").split("\t")
+                   
+                else:
+                    print('Not a valid picard tools output file, must be output from CollectAlignmentSummaryMetrics, CollectInsertSizeMetrics, or summary metrics from CollectGcBiasMetrics')        
+                        
         data_dictionary = dict(zip(header,row)) #create dictionary of header and row
         json_table = json.dumps(data_dictionary) #create json from dictionary
         alignment_metrics_dict = dict({'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'json': json_table})            
         return(alignment_metrics_dict)
 
-    def insertmetrics_parse(self, file):
-        f = open(file, 'r', encoding = "ISO-8859-1")           
-        contents = f.readlines()    
-        for i, content in enumerate(contents):
-            if 'METRICS CLASS' in content:
-                header = (contents[i+1]).strip("\n").split("\t")
-                row = (contents[i+2]).strip("\n").split("\t")
-                qc_program = content.strip("\n").split(".")[-3].split("\t")[-1] 
-                qc_metric = content.strip("\n").split(".")[-1]     
-        data_dictionary = dict(zip(header,row)) #create dictionary of header and row
-        json_table = json.dumps(data_dictionary) #create json from dictionary
-        insert_metrics_dict = dict({'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'json': json_table})            
-        return(insert_metrics_dict)
-  
-          
-    def gcbias_parse(self, file):
-        f = open(file, 'r', encoding = "ISO-8859-1")           
-        contents = f.readlines()
-        for i, content in enumerate(contents):
-            if 'METRICS CLASS' in content:
-                header = (contents[i+1]).strip("\n").split("\t")
-                row = (contents[i+2]).strip("\n").split("\t")
-                qc_program = content.strip("\n").split(".")[-3].split("\t")[-1] 
-                qc_metric = content.strip("\n").split(".")[-1]                  
-        data_dictionary = dict(zip(header,row)) #create dictionary of header and row
-        json_table = json.dumps(data_dictionary) #create json from dictionary
-        gcbias_metrics_dict = dict({'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'json': json_table})            
-        return(gcbias_metrics_dict)
 
