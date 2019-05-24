2bb8fd73a4dd311226a6204e9b5bcfefcca76521
Joselynn Wallace <joselynnw@gmail.com>
2019-05-24 16:51:45 -0400

    refactor(parsers): picardparse.py now properly parses data as a json table and uses BaseParser

--- qcdb/parsers/picardtools_parse.py
+++ qcdb/parsers/picardtools_parse.py
@@ -7,46 +7,35 @@ Created on Mon Apr 15 13:36:11 2019
 """
 
 from qcdb.parsers.parse import BaseParser
-import json, logging, os, glob2
-
+import json, logging, os, glob2, re
 
 log = logging.getLogger(__name__)
-dirname = os.path.dirname(__file__)
 
 class picardtoolsParser(BaseParser):
+    
     def __init__(self, file_handle):
         log.info("Initializing picardtoolsParser...")
         BaseParser.__init__(self,file_handle)
-        
-        self.parse(file_handle)
+        programs = ['alignment_metrics_picard.txt', 'insertsize_metrics_picard.txt', 'summary_gcbias_metrics_picard.txt']        
+        self.parse(programs, os.path.dirname(file_handle))
 
-    def parse(self, file, directory):
-        files = glob2.glob(os.path.join(directory, *'_picard.txt'))
-        for file in files:    
-            f = open(file, 'r', encoding = "ISO-8859-1")           
-            contents = f.readlines()    
-            for i, content in enumerate(contents):
-                if 'METRICS CLASS' in content:
-                    qc_program = content.strip("\n").split(".")[-3].split("\t")[-1] 
-                    qc_metric = content.strip("\n").split(".")[-1]    
-                    header = (contents[i+1]).strip("\n").split("\t")
-                
-                    if qc_metric == 'AlignmentSummaryMetrics':
-                        if 'PAIR' in content:
-                            row = content.strip("\n").split("\t")
-                        elif 'UNPAIRED' in content:
-                            row = content.strip("\n").split("\t")   
-                        
-                    elif (qc_metric == 'InsertSizeMetrics') or (qc_metric == 'GcBiasSummaryMetrics'):
-                        row = (contents[i+2]).strip("\n").split("\t")
-                   
-                    else:
-                        print('Not a valid picard tools output file, must be output from CollectAlignmentSummaryMetrics, CollectInsertSizeMetrics, or summary metrics from CollectGcBiasMetrics')        
-                        
-            data_dictionary = dict(zip(header,row)) #create dictionary of header and row
-            json_table = json.dumps(data_dictionary) #create json from dictionary
-            picard_dict = dict({'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'json': json_table})
+    def parse(self, programs, directory):
+        for program in programs:
+            files = glob2.glob(os.path.join(directory, '*'+program))
+            for file in files:
+                f = open(file, 'r', encoding = "ISO-8859-1")           
+                content = f.read()
+                m = re.search(r"METRICS\sCLASS\s*(\w+)\.[^.]*\.(\w+)\n([^\n]+)\n(([^\n#]+\n)+)", content)
+                qc_program = m.group(1)
+                qc_metric = m.group(2)
+                header = m.group(3)
+                d = m.group(4)
+                p = re.search(r"^(PAIR|UNPAIRED)[^\n]*", m.group(4), flags=re.M)
+                if p:
+                    d = p.group(0)
+                data = d.strip("\n").split("\t")
+                data_dictionary = dict(zip(header, data))
+                json_table = json.dumps(data_dictionary)
+                picard_dict = dict(
+                    {'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'json': json_table})
             self.metrics.append(picard_dict)
-            
-
-
