fab464a6c42ad082f3aced82f74880c127a4fc8e
Joselynn Wallace <joselynnw@gmail.com>
2019-05-28 21:31:39 -0400

    refactor(parsers,-db_load,-reference.yaml): fixed picardtools_parse file dictionary, added information to reference.yaml, added db_load code for picardtoolsParse

--- qcdb/parsers/picardtools_parse.py
+++ qcdb/parsers/picardtools_parse.py
@@ -1,34 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 15 13:36:11 2019
-
-@author: jwalla12
-"""
-
 from qcdb.parsers.parse import BaseParser
 import json, logging, os, glob2, re
 
 log = logging.getLogger(__name__)
+dirname = os.path.dirname('__file__')
 
 class picardtoolsParser(BaseParser):
     
     def __init__(self, file_handle):
         log.info("Initializing picardtoolsParser...")
-        BaseParser.__init__(self,file_handle)
-        programs = ['alignment_metrics_picard.txt', 'insertsize_metrics_picard.txt', 'summary_gcbias_metrics_picard.txt']        
-        self.parse(programs, os.path.dirname(file_handle))
+        BaseParser.__init__(self,file_handle,'picard')       
+        file_table_dict = {'insertsizemetrics': 'insertsize_metrics_picard',
+            'alignmentmetrics': 'alignment_summary_metrics_picard', 
+            'gcbiasmetrics': 'summary_gcbias_metrics_picard'}
+        self.parse(file_table_dict, os.path.dirname(file_handle))
 
-    def parse(self, programs, directory):
-        for program in programs:
-            files = glob2.glob(os.path.join(directory, '*'+program))
+    def parse(self, ft_dict, directory):
+        for module, name in ft_dict.items():
+            log.info("Parsing {} into {}...".format(name, module))
+            files = glob2.glob(os.path.join(directory,
+'{}_{}_*{}.txt'.format(self.sample_name,self.experiment,name)))
+
             for file in files:
                 f = open(file, 'r', encoding = "ISO-8859-1")           
                 content = f.read()
                 m = re.search(r"METRICS\sCLASS\s*(\w+)\.[^.]*\.(\w+)\n([^\n]+)\n(([^\n#]+\n)+)", content)
-                qc_program = m.group(1)
-                qc_metric = m.group(2)
-                header = m.group(3)
+                #module = m.group(2)
+                header = m.group(3).split("\t")
                 d = m.group(4)
                 p = re.search(r"^(PAIR|UNPAIRED)[^\n]*", m.group(4), flags=re.M)
                 if p:
@@ -37,5 +34,5 @@ class picardtoolsParser(BaseParser):
                 data_dictionary = dict(zip(header, data))
                 json_table = json.dumps(data_dictionary)
                 picard_dict = dict(
-                    {'sample_id': self.sample_id, 'qc_program': qc_program, 'qc_metric': qc_metric, 'data': json_table})
-            self.metrics.append(picard_dict)
+                    {'sample_id': self.sample_id, 'qc_program': 'picard', 'qc_metric': module, 'data': json_table})
+                self.metrics.append(picard_dict)
