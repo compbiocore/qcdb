8e01d6e9b768b5d209eab4f9d76224ecbd1e3df1
fernandogelin <fernandogelin@gmail.com>
2019-05-30 15:26:51 -0400

    fix(travis):

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -9,15 +9,16 @@ if not os.getenv('TRAVIS'):
     from dotenv import load_dotenv
     load_dotenv()
 
-
-def test_connection():
-
-    params = {'user': 'root',
+@pytest.fixture
+def params():
+    params_ = {'user': 'root',
               'password': 'password',
               'host': '0.0.0.0',
               'raise_on_warnings': True,
               }
+    return params_
 
+def test_connection(params):
     if os.getenv('TRAVIS'):
         params["password"] = ""
         params["host"] = "127.0.0.1"
@@ -31,17 +32,15 @@ def test_connection():
     assert(con.is_connected() == True)
 
 
-def test_datsci_msql_conn():
-
-    if os.getenv('TRAVIS'):
-        print('Skipping Datasci Server Test')
-    else:
-        params = {'user': os.getenv("MYSQLUSER"),
-                  'password': os.getenv("MYSQLPASSWORD"),
-                  'host': "pdspracticemydbcit.services.brown.edu",
-                  'raise_on_warnings': True,
-                  }
-        con = connection(params)
-
-        assert(type(con) == mysql.connector.connection_cext.CMySQLConnection)
-        assert(con.is_connected() == True)
+# def test_datsci_msql_conn(params):
+#     if os.getenv('TRAVIS'):
+#         print('Skipping Datasci Server Test')
+#     else:
+#         params["user"] = os.getenv("MYSQLUSER")
+#         params["password"] = os.getenv("MYSQLPASSWORD")
+#         params["host"] = "pdspracticemydbcit.services.brown.edu"
+#
+#         con = connection(params)
+#
+#         assert(type(con) == mysql.connector.connection_cext.CMySQLConnection)
+#         assert(con.is_connected() == True)
